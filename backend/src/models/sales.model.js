00e3ed47735b8aa8f7bfff15d3557a0cd9de6f47
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-28 17:36:17 -0300

    ✨ feat: create sales model

---
+++ backend/src/models/sales.model.js
@@ -0,0 +1,21 @@
+const camelize = require('camelize');
+const connection = require('./connection');
+
+const findAll = async () => {
+  const query = `SELECT s.date, sp.* FROM sales_products AS sp 
+  INNER JOIN sales AS s`;
+  const [sales] = await connection.execute(query);
+  return camelize(sales);
+};
+
+const findById = async (saleId) => {
+  const query = `SELECT s.date, sp.product_id, sp.quantity FROM sales AS s 
+  INNER JOIN sales_products AS sp
+  WHERE s.id = ?
+  ORDER BY sp.sale_id, sp.product_id;`;
+  const [sales] = await connection.execute(query, [saleId]);
+
+  return camelize(sales);
+};
+
+module.exports = { findAll, findById };
--- backend/src/routes/sales.route.js
+++ backend/src/routes/sales.route.js
@@ -1,10 +1,8 @@
 const route = require('express').Router();
+
 // const { salesController } = require('../controllers');
 
-route.get('/', (req, res) => res.status(200).end());
-route.get('/:id', (req, res) => res.status(200).end());
-
-// route.get('/', (req, res) => res.status(200).end());
+// route.get('/', salesController.findAll);
 // route.get('/:id', salesController.findById);
 
 module.exports = route;
